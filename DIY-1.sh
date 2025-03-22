f84b14003011638ce1e83b273e3f9de5e919fc77
thungnoon <kongzheng.zhou@gmail.com>
2025-03-22 17:13:08 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -16,3 +16,6 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 5-添加 nikki 插件
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
