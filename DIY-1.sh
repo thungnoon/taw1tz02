1d8f0555bf2b1dbe4f570f63d970e22b178cb7de
thungnoon <kongzheng.zhou@gmail.com>
2025-03-22 20:25:00 +0800

    Add files via upload

--- DIY-1.sh
+++ DIY-1.sh
@@ -7,18 +7,15 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 opentomcat 主题
-git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
-# 4-添加 OpenClash 插件
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-
-# 5-添加 PassWall 插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
-# 6-添加 nikki 插件
+# 3-添加 nikki 插件
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 
-# 7-添加 homeproxy 插件
-sed -i '$a\src-git homeproxy https://github.com/immortalwrt/homeproxy' ./feeds.conf.default 
+# 4-添加 homeproxy 插件
+sed -i '$a\src-git homeproxy https://github.com/immortalwrt/homeproxy' ./feeds.conf.default
+
+# 5-添加 OpenClash 插件
+sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+
+# 6-添加 PassWall 插件
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
