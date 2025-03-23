3f1a49f4416c1068c5c401483d9fa89da9280f2a
thungnoon <kongzheng.zhou@gmail.com>
2025-03-23 17:30:12 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -38,6 +38,8 @@ git clone -b main --depth 1 https://github.com/linkease/istore.git
 git clone -b main --depth 1 https://github.com/linkease/istore-ui.git
 # #nikki 
 git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+# #luci-theme-infinityfreedom
+git clone -b main --depth 1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
 popd
 
 echo "packages executed successfully!"
