3222a2da522985d05063ad20a2da7106bf9ecde3
thungnoon <kongzheng.zhou@gmail.com>
2025-03-24 17:31:56 +0800

    Delete scripts/init-settings.sh

--- scripts/init-settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#修改内核版本
-# sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
-
-#更改默认地址为192.168.5.1
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-
-echo "init-settings executed successfully!"
